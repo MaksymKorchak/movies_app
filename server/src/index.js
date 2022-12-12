6c690c6820ae7aa19a687f82b4e8a696675bebb2
Maksym Korchak <Mkorchak1992@gmail.com>
2022-12-12 23:25:32 +0200

    Rollback changes from server folder

--- server/src/index.js
+++ server/src/index.js
@@ -1,60 +1,57 @@
-const { ApolloServer } = require('apollo-server-express');
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-
-const {
-  ApolloServerPluginDrainHttpServer,
-  ApolloServerPluginLandingPageLocalDefault,
-} = require('apollo-server-core');
-
-const Query = require('./resolvers/Query')
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { ApolloServerPluginDrainHttpServer } = require("@apollo/server/plugin/drainHttpServer");
+const { json } = require("body-parser");
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const cors = require("cors");
+const Query = require("./resolvers/Query");
 
 const resolvers = {
-  Query
-}
+  Query,
+};
 
 const typeDefs = fs.readFileSync(
-  path.join(__dirname, 'schema.graphql'),
-  'utf8'
-);
-
-const context = ({ req, res }) => ({
-  locale: req?.headers?.locale || 'en-US'
-})
-
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  );
 
 async function startApolloServer(typeDefs, resolvers) {
   const app = express();
   const httpServer = http.createServer(app);
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context,
-    csrfPrevention: true,
-    cache: 'bounded',
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-  await server.start();
-  server.applyMiddleware({ app });
 
-  app.use(express.static(path.join(__dirname, '../../client', 'build')));
+  await server.start();
+  
+  app.use(
+    "/graphql",
+    cors(),
+    json(),
+    expressMiddleware(server, {
+        context: async ({ req }) => ({ locale: req?.headers?.locale || "en-US" }),
+    })
+  );
+
+  app.use(express.static(path.join(__dirname, "../../client", "build")));
   app.use(express.static("public"));
 
-  app.get('/rest', function (req, res) {
-    res.json({data: 'rest works'})
-  })
-
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../client', 'build', 'index.html'))
+  app.get("/rest", (req, res) => {
+    res.json({ data: "rest" });
   });
 
-  await new Promise(resolve => httpServer.listen({ port: process.env.PORT || 80 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 80}${server.graphqlPath}`);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client", "build", "index.html"));
+  });
+
+  await new Promise((resolve) => httpServer.listen({ port: process.env.PORT || 80  }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 80}/graphql`);
 }
 
 startApolloServer(typeDefs, resolvers);
